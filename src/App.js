db236320482eb4f2778c0509ce85717001025378
Helder Avila <helderavila2016@gmail.com>
2021-09-27 20:17:44 -0300

    Create a button to call contract and mint NFT

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
+import { ethers } from 'ethers'
 import "./styles/App.css";
 import twitterLogo from "./assets/twitter-logo.svg";
+import EpicNFT from './utils/EpicNFT.json'
 
 // Constants
 const TWITTER_HANDLE = "_buildspace";
@@ -63,6 +65,34 @@ const App = () => {
     }
   }
 
+  const askContractToMintNft = async () => {
+    const CONTRACT_ADDRESS = "0x030d6e7bA8A3ee477330471cCc6F9847bc1047A4";
+      try {
+        const { ethereum } = window;
+  
+        if (ethereum) {
+          const provider = new ethers.providers.Web3Provider(ethereum);
+          const signer = provider.getSigner();
+          const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, EpicNFT.abi, signer);
+  
+          console.log("Going to pop wallet now to pay gas...")
+          let nftTxn = await connectedContract.makeAnEpicNFT();
+  
+          console.log("Mining...please wait.")
+          await nftTxn.wait();
+          
+          console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`);
+  
+        } else {
+          console.log("Ethereum object doesn't exist!");
+        }
+      } catch (error) {
+        console.log(error)
+      }
+  }
+  
+  
+
 
   const renderNotConnectedContainer = () => (
     <button onClick={connectWallet} className="cta-button connect-wallet-button">
@@ -74,7 +104,7 @@ const App = () => {
   * We want the "Connect to Wallet" button to dissapear if they've already connected their wallet!
   */
   const renderMintUI = () => (
-    <button onClick={null} className="cta-button connect-wallet-button">
+    <button onClick={askContractToMintNft} className="cta-button connect-wallet-button">
       Mint NFT
     </button>
   )
