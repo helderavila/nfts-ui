3176572539ae815bdbe3e0a34b2b0059cfdba595
Helder Avila <helderavila2016@gmail.com>
2021-09-27 22:48:29 -0300

    Add event listener to mint

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from "react";
 import { ethers } from 'ethers'
 import "./styles/App.css";
@@ -7,12 +8,15 @@ import EpicNFT from './utils/EpicNFT.json'
 // Constants
 const TWITTER_HANDLE = "_buildspace";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
-const OPENSEA_LINK = "";
-const TOTAL_MINT_COUNT = 50;
+const CONTRACT_ADDRESS = "0xFe50b9282a50651BfAE8c37A10Fd2F0B83bec6b7";
+const OPENSEA_LINK = `https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/`;
+// const TOTAL_MINT_COUNT = 50;
 
 const App = () => {
   const [currentAccount, setCurrentAccount] = useState("");
   const [isMinting, setIsMinting] = useState(false)
+  const [mintSuccess, setMintSuccess] = useState(false)
+  const [tokenId, setTokenId] = useState()
 
   // Render
   const checkIfWalletIsConnected = async () => {
@@ -37,6 +41,7 @@ const App = () => {
       const account = accounts[0];
       console.log("Found an authorized account:", account);
       setCurrentAccount(account);
+      setupEventListener()
     } else {
       console.log("No authorized account found");
     }
@@ -61,13 +66,13 @@ const App = () => {
       */
       console.log("Connected", accounts[0]);
       setCurrentAccount(accounts[0]); 
+      setupEventListener()
     } catch (error) {
       console.log(error)
     }
   }
 
   const askContractToMintNft = async () => {
-    const CONTRACT_ADDRESS = "0x030d6e7bA8A3ee477330471cCc6F9847bc1047A4";
     setIsMinting(true)
       try {
         const { ethereum } = window;
@@ -85,17 +90,45 @@ const App = () => {
           
           console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`);
           setIsMinting(false)
+          setMintSuccess(true)
         } else {
           console.log("Ethereum object doesn't exist!");
         }
       } catch (error) {
         console.log(error)
         setIsMinting(false)
+        setMintSuccess(false)
       }
   }
   
-  
+  const setupEventListener = async () => {
+    // Most of this looks the same as our function askContractToMintNft
+    try {
+      const { ethereum } = window;
 
+      if (ethereum) {
+        // Same stuff again
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, EpicNFT.abi, signer);
+
+        // THIS IS THE MAGIC SAUCE.
+        // This will essentially "capture" our event when our contract throws it.
+        // If you're familiar with webhooks, it's very similar to that!
+        connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
+          console.log(from, tokenId.toNumber())
+          setTokenId(tokenId.toNumber())
+        });
+
+        console.log("Setup event listener!")
+
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   const renderNotConnectedContainer = () => (
     <button onClick={connectWallet} className="cta-button connect-wallet-button">
@@ -112,6 +145,12 @@ const App = () => {
     </button>
   )
 
+  const renderMintedUI = () => (
+      <a target="_blank" href={`${OPENSEA_LINK}/${tokenId}`} className="cta-button view-nft-button" rel="noreferrer">
+      Opensea
+    </a>
+  )
+
   useEffect(() => {
     checkIfWalletIsConnected();
   }, []);
@@ -124,7 +163,10 @@ const App = () => {
           <p className="sub-text">
             Each unique. Each beautiful. Discover your NFT today.
           </p>
-          {currentAccount === "" ? renderNotConnectedContainer() : renderMintUI()}
+          <div className="buttons-container">
+            {currentAccount === "" ? renderNotConnectedContainer() : renderMintUI()}
+            {mintSuccess && renderMintedUI()}
+          </div>
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
